19549eec8eaafe1803f3012f8bec8f6cf206462f
Aiman Sulaiman <masmate87@gmail.com>
2020-04-26 13:58:32 +0800

    Create Quest2.js

---
+++ Quest2.js
@@ -0,0 +1,45 @@
+//# Coding-test-Aiman
+//Question2
+var equation = "a-b-c-d" 
+var dictOfVariableValues = {a:1,b:2,c:3,d:4}
+
+
+function calculateEquation(equation,dictOfVariableValues) {
+    var finalValue = 0
+    //changes strings to arr
+    arrOfStr = equation.split("") //["a", "-", "b", "-", "c", "-", "d"]
+    
+    if ( (arrOfStr[0] !== "-")   && (arrOfStr[0] !== "+")) {
+        finalValue =positiveStart(arrOfStr,dictOfVariableValues);
+        return finalValue
+    } else {
+        finalValue =signStart(arrOfStr,dictOfVariableValues)
+        return finalValue
+    }
+}
+
+function positiveStart(arrOfStr) {
+    var objOne = dictOfVariableValues;
+    var totalSum = 0
+    for (var i=0;i<arrOfStr.length;i+=2) {
+        if (arrOfStr[i-1] === "-") {
+            totalSum -= objOne[arrOfStr[i]] 
+        } else {
+            totalSum +=objOne[arrOfStr[i]] 
+        }   
+    }
+    return totalSum
+}
+
+function signStart(arrOne) {
+    var objOne = dictOfVariableValues;
+    var totalSum = 0
+    for (var i=1;i<arrOfStr.length;i+=2) {
+        if (arrOfStr[i-1] === "-") {
+            totalSum -= objOne[arrOfStr[i]] 
+        } else {
+            totalSum +=objOne[arrOfStr[i]] 
+        }   
+    }
+    return totalSum
+}
